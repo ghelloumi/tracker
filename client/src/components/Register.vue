bb1635f79883966a4dfc4d35b7f3c2726b908743
Ghassen Elloumi <ell.ghassen@gmail.com>
2018-11-22 13:55:05 +0100

    first commit

---
+++ client/src/components/Register.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" placeholder="Email" v-model="email"/><br/>
+    <input type="password" name="password" placeholder="Password" v-model="password"/><br/>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+  import AuthenticationService from '@/services/AuthenticationService'
+
+  export default {
+    data() {
+      return {
+        email: "",
+        password: "",
+      }
+    },
+    watch: {
+      email(value) {
+        console.log("email changer value to ", value)
+      }
+    },
+    methods: {
+      async register() {
+        try {
+          await AuthenticationService.register({
+            email: this.email,
+            password: this.password
+          })
+        } catch (e) {
+          console.error(e)
+        }
+      }
+    },
+    mounted() {
+      console.log('Hello')
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
